67357a9c295184dc8d31752c387e9c4baf29cb4c
stefano-b <bartoletti.stefano@gmail.com>
2019-02-07 16:09:21 +0100

    page 404

---
+++ 404.php
@@ -0,0 +1,29 @@
+<?php get_header() ?>
+
+<!-- main content wrapper -->
+
+<main class="content-wrap">
+
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <article class="col-sm-8">
+
+                <h1 class="mb-5 text-center">
+                    <?php esc_html_e( 'Oops, that page can&#39;t be found', 'sb' ); ?>
+                </h1>
+
+                <h2 class="mb-5 text-center">
+                    <?php esc_html_e( 'Error 404', 'sb-base-theme' ); ?>
+                </h2>
+
+            </article>
+
+        </div>
+
+    </div>
+
+</main>
+
+<?php get_footer() ?>
