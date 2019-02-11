9aa67dfdd0838afd68d5e0b37f5ef6fc7abb81b6
stefano-b <bartoletti.stefano@gmail.com>
2019-02-11 20:11:14 +0100

    Homepage with partial sections

--- front-page.php
+++ front-page.php
@@ -1,23 +1,49 @@
 <?php get_header() ?>
 
-<main class="content-wrap container">
+<main class="content-wrap">
 
-    <div class="row">
-        <div class="col">
-            <h1>Test</h1>
+<section class="section-wrap element-slider">
 
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi obcaecati officia reprehenderit hic.
-                Nobis hic quis officiis labore consequuntur soluta facere ratione facilis? Eum, nobis quae error earum
-                aperiam eligendi!</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi obcaecati officia reprehenderit hic.
-                Nobis hic quis officiis labore consequuntur soluta facere ratione facilis? Eum, nobis quae error earum
-                aperiam eligendi!</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi obcaecati officia reprehenderit hic.
-                Nobis hic quis officiis labore consequuntur soluta facere ratione facilis? Eum, nobis quae error earum
-                aperiam eligendi!</p>
-        </div>
-    </div>
+    <?php $sb_slider_args = array(
+        'post_type' => 'post',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'category',
+                'field'    => 'slug',
+                'terms'    => 'slider',
+            ),
+        ),
+        'posts_per_page'    => 3,
+    );
+    
+    set_query_var( 'sb_slider_args', $sb_slider_args );
+        
+    get_template_part( 'template-parts/section', 'slider' ); ?>
 
+</section>
+
+<section class="section-wrap element-cards container py-5">
+
+    <h2>News</h2>
+
+    <?php $sb_cards_args = array(
+        'post_type' => 'post',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'category',
+                'field'    => 'slug',
+                'terms'    => 'news',
+            ),
+        ),
+        'posts_per_page'    => 3,
+    );
+    
+    set_query_var( 'sb_cards_args', $sb_cards_args );
+        
+    get_template_part( 'template-parts/section', 'cards' ); ?>
+
+</section>
+ 
 </main>
 
 <?php get_footer() ?>
