2fd640eae3ac40be5f224e22d1858aa5682dd1ad
stefano-b <bartoletti.stefano@gmail.com>
2019-02-14 18:48:12 +0100

    Included local bootstrap

--- functions.php
+++ functions.php
@@ -79,7 +79,7 @@ add_action('widgets_init', 'sb_sidebars');
 /* Include assets
 ----------------------------------------------------------------------------------------*/
 
-require_once get_template_directory() . '/assets/class-wp-bootstrap-navwalker.php';
+require_once get_template_directory() . '/assets/navwalker/class-wp-bootstrap-navwalker.php';
 
 
 /* Include CSS & JavaScript
@@ -89,15 +89,15 @@ if(! function_exists('sb_styles_scripts')) {
 
     function sb_styles_scripts() {
 
-      wp_enqueue_style('bootstrap-css', '//stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css');
+      // wp_enqueue_style('bootstrap-css', '//stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css');
 
-      wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Muli:400,700');
+      // wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Muli:400,700');
 
       wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.6.3/css/all.css');
 
       wp_enqueue_style('sb-custom-css', get_template_directory_uri() .'/css/style.min.css');
 
-      wp_enqueue_script('sb-bootstrap-bundle-js', '//stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js', array('jquery'), null, true );
+      wp_enqueue_script('sb-bootstrap-bundle-js', '//stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.bundle.min.js', array('jquery'), null, true );
 
       if ( is_singular() && comments_open() && get_option('thread_comments') ) { wp_enqueue_script('comment-reply'); }
 
