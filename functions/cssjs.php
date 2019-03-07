d5a6105d7fba71e4806ead6767ecf624a0768343
stefano-b <bartoletti.stefano@gmail.com>
2019-03-07 17:12:52 +0100

    Folder structure

--- functions/cssjs.php
+++ functions/cssjs.php
@@ -12,7 +12,7 @@ if(! function_exists('sb_styles_scripts')) {
 
         wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.6.3/css/all.css');
 
-        wp_enqueue_style('sb-custom-css', get_template_directory_uri() .'/css/style.min.css');
+        wp_enqueue_style('sb-custom-css', get_template_directory_uri() .'/dist/css/style.min.css');
 
         // JS
 
@@ -20,7 +20,7 @@ if(! function_exists('sb_styles_scripts')) {
 
         if ( is_singular() && comments_open() && get_option('thread_comments') ) { wp_enqueue_script('comment-reply'); }
 
-        wp_enqueue_script('sb-custom-js', get_template_directory_uri() .'/js/scripts.min.js', array('jquery'), null, true );
+        wp_enqueue_script('sb-custom-js', get_template_directory_uri() .'/dist/js/scripts.min.js', array('jquery'), null, true );
 
     }
 
