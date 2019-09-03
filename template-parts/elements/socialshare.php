576f9b7e81b3d736a8b627ce334e4175b8d75f53
stefano-b <bartoletti.stefano@gmail.com>
2019-09-03 15:13:00 +0200

    button test

---
+++ template-parts/elements/socialshare.php
@@ -0,0 +1,24 @@
+<div class="socialshare py-3">
+
+    <button type="button" class="btn btn-primary">
+        <i class="fab fa-facebook-f"></i><span>Facebook</span>
+    </button>
+
+    <button type="button" class="btn btn-secondary">
+        <i class="fab fa-facebook-f"></i><span>Facebook</span>
+    </button>
+
+    <button type="button" class="btn btn-success">
+        <i class="fab fa-facebook-f"></i><span>Facebook</span>
+    </button>
+
+    <button type="button" class="btn btn-warning">
+        <i class="fab fa-facebook-f"></i><span>Facebook</span>
+    </button>
+
+    <button type="button" class="btn btn-danger">
+        <i class="fab fa-facebook-f"></i><div>Facebook</div>
+    </button>
+
+
+</div>
