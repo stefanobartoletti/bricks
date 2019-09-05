47a651abae2a56d231c6dab603438814f32c6e88
stefano-b <bartoletti.stefano@gmail.com>
2019-09-05 15:48:04 +0200

    HTML in loop

--- template-parts/elements/socialshare.php
+++ template-parts/elements/socialshare.php
@@ -15,7 +15,7 @@ $shareurl_facebook = 'https://www.facebook.com/shareurl.php?u='.$url;
 $shareurl_twitter = 'https://twitter.com/intent/tweet?url='.$url.'&text='.$title;
 
 
-// --- Social Networks Class ---
+// --- Social Network Class ---
 
 class sb_social_network {
 
@@ -40,48 +40,22 @@ $sb_socials[] = new sb_social_network('Facebook', 'fab fa-facebook-f', $shareurl
 $sb_socials[] = new sb_social_network('Twitter', 'fab fa-twitter', $shareurl_twitter);
 
 
-
-foreach ($sb_socials as $sb_social) {
-    echo '<p>This is <b>' . strtolower($sb_social->sb_social_name) . '</b>, its icon is called <b>' . $sb_social->sb_social_icon . '</b> and its URL is <b>' . $sb_social->sb_social_shareurl . '</b>.';
-}
-
-
+// --- Social Buttons HTML ---
 
 ?>
-
 
 <div class="sharebuttons py-3">
 
     <div class="btn">
         <i class="fas fa-share-alt"></i>
     </div>
-    
-    <a class="btn socialshare share-facebook" href="#" role="button">
-        <i class="fab fa-facebook-f"></i>
-    </a>
 
-    <a class="btn socialshare share-twitter" href="#" role="button">
-        <i class="fab fa-twitter"></i>
-    </a>
+    <?php foreach ($sb_socials as $sb_social) { ?>
 
-    <a class="btn socialshare share-linkedin" href="#" role="button">
-        <i class="fab fa-linkedin-in"></i>
-    </a>
+        <a class="btn socialshare share-<?php echo strtolower($sb_social->sb_social_name) ?>" href="<?php echo $sb_social->sb_social_shareurl ?>" role="button">
+            <i class="<?php echo $sb_social->sb_social_icon ?>"></i>
+        </a>
 
-    <a class="btn socialshare share-pinterest" href="#" role="button">
-        <i class="fab fa-pinterest-p"></i>
-    </a>
-
-    <a class="btn socialshare share-pocket" href="#" role="button">
-        <i class="fab fa-get-pocket"></i>
-    </a>
-
-    <a class="btn socialshare share-telegram" href="#" role="button">
-        <i class="fab fa-telegram-plane"></i>
-    </a>
-
-    <a class="btn socialshare share-email" href="#" role="button">
-        <i class="fas fa-envelope"></i>
-    </a>
+    <?php } ?>
 
 </div>
