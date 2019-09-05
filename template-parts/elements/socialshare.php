4c44351a8832d3e91632cf40c2f71b37df8cfeff
stefano-b <bartoletti.stefano@gmail.com>
2019-09-05 13:32:52 +0200

    shareurls

--- template-parts/elements/socialshare.php
+++ template-parts/elements/socialshare.php
@@ -1,70 +1,83 @@
 <?php 
 
-class sb_share {
+// --- Social Share URLs ---
 
-    public $sb_share_name;
-    public $sb_share_color;
-    public $sb_share_icon;
-    public $sb_share_url;
+// https://github.com/bradvin/social-share-urls
 
-    public function __construct($name, $color, $icon, $url) {
-        $this->sb_share_name = $name;
-        $this->sb_share_color = $color;
-        $this->sb_share_icon = $icon;
-        $this->sb_share_url = $url;
+// URL arguments
+
+$url = get_the_permalink();
+
+// Share URLs
+
+$shareurl_facebook = 'https://www.facebook.com/shareurl.php?u=' . $url;
+
+
+// --- Social Networks Class ---
+class sb_social_network {
+
+    public $sb_social_name;
+    public $sb_social_icon;
+    public $sb_social_shareurl;
+
+    public function __construct($sb_social_name, $sb_social_icon, $sb_social_shareurl) {
+        $this->sb_social_name = $sb_social_name;
+        $this->sb_social_icon = $sb_social_icon;
+        $this->sb_social_shareurl = $sb_social_shareurl;
       }
 
     public function share_button() {
-        echo '<p>This is ' . $this->sb_share_name . ', its brand color is ' . $this->sb_share_color . ', its icon is called ' . $this->sb_share_icon . ' and its URL is ' . $this->sb_share_url . '.';
+        echo '<p>This is ' . $this->sb_social_name . ', its icon is called ' . $this->sb_social_icon . ' and its URL is ' . $this->sb_social_shareurl . '.';
     }
     
 }
 
-$sb_share_facebook = new sb_share(
-    'Facebook',
-    '#3b5998',
-    'facebook-f',
-    'https://www.facebook.com/sharer.php?u=' . get_the_permalink()
+// --- Social Networks Objects ---
+
+$sb_social_facebook = new sb_social_network(
+    'facebook',
+    'fab fa-facebook-f',
+    $shareurl_facebook
 );
 
-$sb_share_facebook->share_button();
+$sb_social_facebook->share_button();
 
 // https://stackoverflow.com/a/8612210/10580177
 
 ?>
 
 
-<div class="socialshare py-3">
+<div class="sharebuttons py-3">
 
     <div class="btn">
         <i class="fas fa-share-alt"></i>
     </div>
     
-    <a class="btn" href="#" role="button" style="background-color: #3b5998">
+    <a class="btn socialshare share-facebook" href="#" role="button">
         <i class="fab fa-facebook-f"></i>
     </a>
 
-    <a class="btn" href="#" role="button" style="background-color: #1da1f2">
+    <a class="btn socialshare share-twitter" href="#" role="button">
         <i class="fab fa-twitter"></i>
     </a>
 
-    <a class="btn" href="#" role="button" style="background-color: #0077b5">
+    <a class="btn socialshare share-linkedin" href="#" role="button">
         <i class="fab fa-linkedin-in"></i>
     </a>
 
-    <a class="btn" href="#" role="button" style="background-color: #bd081c">
+    <a class="btn socialshare share-pinterest" href="#" role="button">
         <i class="fab fa-pinterest-p"></i>
     </a>
 
-    <a class="btn" href="#" role="button" style="background-color: #ef4056">
+    <a class="btn socialshare share-pocket" href="#" role="button">
         <i class="fab fa-get-pocket"></i>
     </a>
 
-    <a class="btn" href="#" role="button" style="background-color: #0088cc">
+    <a class="btn socialshare share-telegram" href="#" role="button">
         <i class="fab fa-telegram-plane"></i>
     </a>
 
-    <a class="btn" href="#" role="button" style="background-color: #777777">
+    <a class="btn socialshare share-email" href="#" role="button">
         <i class="fas fa-envelope"></i>
     </a>
 
