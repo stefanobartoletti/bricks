9bc36322f3d05862b1320de98f8aac30fa4d2ead
stefano-b <bartoletti.stefano@gmail.com>
2019-09-20 19:23:51 +0200

    searchform

--- searchform.php
+++ searchform.php
@@ -4,19 +4,10 @@ $searchtext = esc_html__( 'Search', 'sb-base-theme' );
 
 ?>
 
-<form class="" action="<?php echo esc_url_raw(home_url()); ?>" method="get">
+<form class="form-inline" action="<?php echo esc_url_raw(home_url()); ?>" method="get">
 
-    <div class="form-row">
-
-        <div class="col-auto">
-            <input class="form-control" type="search" placeholder="<?php echo $searchtext ?>"
-                aria-label="<?php echo $searchtext ?>" name="s">
-        </div>
-
-        <div class="col-2">
-            <button class="btn btn-outline-dark" type="submit"><?php echo $searchtext ?></button>
-        </div>
-
-    </div>
+    <input class="form-control form-control-sm mr-2" type="search" placeholder="<?php echo $searchtext ?>"
+        aria-label="<?php echo $searchtext ?>" name="s">
+    <button class="btn btn-sm btn-outline-dark" type="submit"><?php echo $searchtext ?></button>
 
 </form>
