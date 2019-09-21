d1cc55c394a994075d599e0aa107eb9cb4e23432
stefano-b <bartoletti.stefano@gmail.com>
2019-09-21 12:11:04 +0200

    single

--- single.php
+++ single.php
@@ -1,38 +1,28 @@
 <?php get_header(); ?>
 
-<!-- main content wrapper -->
-
 <main id="content-wrap">
 
     <?php while ( have_posts() ) : the_post(); ?>
     
     <div class="container">
 
-        <div class="row py-5">
+        <div class="row">
 
-            <!-- blog post wrapper -->
+            <div class="col">
+
+                <?php get_template_part( 'templates/elements/breadcrumbs', '' ); ?>
+
+            </div>
+
+        </div>
+
+        <div class="row py-5">
 
             <div id="article-wrap" class="col">       
 
-                <?php get_template_part( 'templates/elements/breadcrumbs', '' ); ?>
+                <?php get_template_part( 'templates/content/single', '' ); 
                 
-                <article <?php post_class(); ?> >
-
-                    <h1><?php the_title(); ?></h1>
-
-                    <p><?php the_author(); ?> - <?php the_time('j M, Y'); ?></p>
-
-                    <p><?php the_category(', '); ?> - <?php the_tags('', ', ', ''); ?></p>
-                    
-                    <?php the_post_thumbnail('sb_single', array( 'class' => 'img-fluid', 'alt' => get_the_title() )); ?>
-
-                    <div><?php the_content(); ?></div>
-
-                    <?php wp_link_pages('pagelink=Page %'); ?>
-
-                </article>
-
-                <?php get_template_part( 'templates/elements/socialshare', '' ); ?>
+                get_template_part( 'templates/elements/socialshare', '' ); ?>
 
                 <nav class="nav">
                     <?php previous_post_link('<span class="nav-link mr-auto">&laquo; %link</span>');
@@ -41,7 +31,7 @@
                     
                 <?php if ( comments_open() || get_comments_number() ) { comments_template(); } ?>
             
-            </div>
+            </div> <!-- #article-wrap -->
 
             <?php get_sidebar(); ?>
 
@@ -51,6 +41,6 @@
   
     <?php endwhile ?>
 
-</main>
+</main> <!-- #content-wrap -->
 
 <?php get_footer();
