9b4c43363c68c5cf84ee81bd8ef328a51fa6d260
stefano-b <bartoletti.stefano@gmail.com>
2019-09-21 18:54:50 +0200

    icon in search button

--- searchform.php
+++ searchform.php
@@ -8,13 +8,13 @@ $searchtext = esc_html__( 'Search', 'sb-base-theme' );
 
     <div class="form-row">
 
-        <div class="col-auto">
+        <div class="col">
             <input class="form-control form-control-sm" type="search" placeholder="<?php echo $searchtext ?>"
                 aria-label="<?php echo $searchtext ?>" name="s">
         </div>
 
-        <div class="col-2">
-            <button class="btn btn-sm btn-outline-dark" type="submit"><?php echo $searchtext ?></button>
+        <div class="col-3">
+            <button class="btn btn-sm btn-outline-dark" type="submit"><i class="fas fa-search"></i></button>
         </div>
 
     </div>
