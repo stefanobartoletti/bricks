ed3c5b4259708c945f40d081ced6e40c4c005da6
stefano-b <bartoletti.stefano@gmail.com>
2019-09-23 17:49:32 +0200

    footer copyright

--- footer.php
+++ footer.php
@@ -2,7 +2,12 @@
 
     <footer id="footer-wrap" class="bg-dark text-light">
 
-        <?php  get_template_part( 'templates/footer/footer', 'simple' ); ?>
+        <?php
+
+        get_template_part( 'templates/footer/footer', 'simple' ); 
+        get_template_part( 'templates/footer/footer', 'copyright' ); 
+        
+        ?>
 
     </footer> <!-- #footer-wrap -->
 
---
+++ templates/footer/footer-copyright.php
@@ -0,0 +1,16 @@
+<div id="footer-copyright" class="navbar navbar-expand navbar-dark container flex-column flex-md-row small">
+               
+    <span class="navbar-text">
+        <?php echo 
+        "&#169; ".date('o')." - ".
+        get_theme_mod('sb_company')." - ".
+        get_theme_mod('sb_address_1').", ".
+        get_theme_mod('sb_address_2')." - ".
+        "c.f. ".get_theme_mod('sb_cfisc')." - ".
+        "p.i. ".get_theme_mod('sb_piva')
+        ?>
+    </span>
+
+    <a class="navbar-text ml-md-auto" href="https://www.stefanobartoletti.it" target="_blank">Made by [sb]</a>
+   
+</div> <!-- #footer-copyright --> 
