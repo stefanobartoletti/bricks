1a5e8a536c26c2cbfe033f1a5da2d12ae65d6ac8
stefano-b <bartoletti.stefano@gmail.com>
2019-09-23 17:49:04 +0200

    footer simple logo

--- templates/footer/footer-simple.php
+++ templates/footer/footer-simple.php
@@ -1,17 +1,14 @@
 <div id="footer-simple" class="navbar navbar-expand navbar-dark container flex-column flex-md-row my-3">
                
-    <span class="navbar-text">
-        <?php echo 
-        "&#169; ".date('o')." - ".
-        get_theme_mod('sb_company')." - ".
-        get_theme_mod('sb_address_1').", ".
-        get_theme_mod('sb_address_2')." - ".
-        "c.f. ".get_theme_mod('sb_cfisc')." - ".
-        "p.i. ".get_theme_mod('sb_piva')
-        ?>
-    </span>
+    <?php 
+    
+    if ( has_custom_logo() ) { 
 
-    <?php wp_nav_menu(array(
+        the_custom_logo();
+
+    }
+
+    wp_nav_menu(array(
         'theme_location'    => 'footer',
         'depth'             => 1,
         'container'         => 'nav',
@@ -20,6 +17,8 @@
         'menu_class'        => 'footer-menu navbar-nav',
         'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
         'walker'            => new WP_Bootstrap_Navwalker(),
-    )); ?>
+    ));
+    
+    ?>
    
 </div> <!-- #footer-simple --> 
