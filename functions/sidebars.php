9e8e55194c3db148fd0c420f007ac78ff8969fb4
stefano-b <bartoletti.stefano@gmail.com>
2019-09-23 11:44:16 +0200

    register footer sidebar

--- functions/sidebars.php
+++ functions/sidebars.php
@@ -12,10 +12,20 @@ if(! function_exists('sb_sidebars')) {
         register_sidebar(array(
             'name' =>  esc_html__('Main Sidebar', 'sb-base-theme'),
             'id' => 'sidebar-main',
-            'description' => esc_html__( 'Main Sidebar', 'sb-base-theme' ),
+            'description' => esc_html__('Main Sidebar', 'sb-base-theme'),
             'before_widget' => '<div class="widget mb-4 %2$s clearfix">',
             'after_widget' => '</div>',
             'before_title' => '<h3 class="mb-4">',
+            'after_title' => '</h3>',
+        ));
+
+        register_sidebar(array(
+            'name' =>  esc_html__('Footer Widgets', 'sb-base-theme'),
+            'id' => 'sidebar-footer',
+            'description' => esc_html__('Footer Widgets', 'sb-base-theme'),
+            'before_widget' => '<div class="widget col-md-4 mb-4 %2$s clearfix">',
+            'after_widget' => '</div>',
+            'before_title' => '<h3 class="h4 mb-3">',
             'after_title' => '</h3>',
         ));
 
