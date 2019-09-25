ca665970ac22cc0bd904ac4c469c5e5397f8c50d
stefano-b <bartoletti.stefano@gmail.com>
2019-09-25 10:57:03 +0200

    header simple

--- header.php
+++ header.php
@@ -16,47 +16,7 @@
 
     <header id="header-wrap" class="sticky-top bg-dark">
 
-        <nav class="navbar navbar-expand-lg navbar-dark container" role="navigation">
-
-            <?php if ( has_custom_logo() ) { 
-
-                the_custom_logo();
-
-            } else { ?>
-
-                <a class="navbar-brand" href="<?php echo esc_url_raw(home_url()); ?>"><?php bloginfo('name'); ?></a>
-
-            <?php } ?>
-    
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu-header"
-                aria-controls="navbar-menu-header" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div id="navbar-menu-header" class="collapse navbar-collapse">
-
-                <?php
-                
-                wp_nav_menu(array(
-                        'theme_location'    => 'header',
-                        'depth'             => 2,
-                        'container'         => '',
-                        'container_class'   => '',
-                        'container_id'      => '',
-                        'menu_class'        => 'header-menu nav navbar-nav my-3 my-lg-0 ml-lg-2 mr-auto',
-                        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker'            => new WP_Bootstrap_Navwalker(),
-                ));
-                
-                get_search_form();
-                
-                get_template_part( 'templates/elements/socialicons', '' );
-                
-                ?>
-
-            </div>
-
-        </nav>
+        <?php get_template_part('templates/header/header', 'simple') ?>
 
     </header> <!-- #header-wrap -->
 
---
+++ templates/header/header-simple.php
@@ -0,0 +1,40 @@
+<nav id="header-simple" class="navbar navbar-expand-lg navbar-dark container" role="navigation">
+
+    <?php if ( has_custom_logo() ) { 
+
+        the_custom_logo();
+
+    } else { ?>
+
+        <a class="navbar-brand" href="<?php echo esc_url_raw(home_url()); ?>"><?php bloginfo('name'); ?></a>
+
+    <?php } ?>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header-simple-menu" aria-controls="header-simple-menu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div id="header-simple-menu" class="collapse navbar-collapse">
+
+        <?php
+        
+        wp_nav_menu(array(
+                'theme_location'    => 'header',
+                'depth'             => 2,
+                'container'         => '',
+                'container_class'   => '',
+                'container_id'      => '',
+                'menu_class'        => 'header-menu nav navbar-nav my-3 my-lg-0 ml-lg-2 mr-auto',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker(),
+        ));
+        
+        get_search_form();
+        
+        get_template_part( 'templates/elements/socialicons', '' );
+        
+        ?>
+
+    </div>
+
+</nav>
