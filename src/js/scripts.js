50804015f63fdf3da388e319fc4cede33fb0a434
stefano-b <bartoletti.stefano@gmail.com>
2019-10-07 18:07:25 +0200

    JS check if selectors exist

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,51 +9,72 @@ var html = document.querySelector('html');
 
 var header = document.querySelector('#header-wrap');
 
-document.addEventListener('scroll', function() {
-    var scrolled = html.scrollTop;
-    if (scrolled > 50) {
-        header.classList.add('header-scrolled');
-    } else {
-        header.classList.remove('header-scrolled');
-    }
-});
+if (header) {
+
+    document.addEventListener('scroll', function () {
+        var scrolled = html.scrollTop;
+        if (scrolled > 50) {
+            header.classList.add('header-scrolled');
+        } else {
+            header.classList.remove('header-scrolled');
+        }
+    });
+
+};
 
 
 // --- Back to Top ---
 
 var backToTop = document.querySelector('#backtotop');
 
-document.addEventListener('scroll', function() {
-    var scrolled = html.scrollTop;
-    if (scrolled > 200) {
-        backToTop.classList.remove('hidden');
-    } else {
-        backToTop.classList.add('hidden');
-    }
-});
+if (backToTop) {
 
-backToTop.addEventListener('click', function() {
-    window.scrollTo(0, 0);
-});
+    document.addEventListener('scroll', function () {
+        var scrolled = html.scrollTop;
+        if (scrolled > 200) {
+            backToTop.classList.remove('hidden');
+        } else {
+            backToTop.classList.add('hidden');
+        }
+    });
+
+    backToTop.addEventListener('click', function () {
+        window.scrollTo(0, 0);
+    });
+
+};
 
 
 // --- Bootstrap classes to Wordpress code ---
 
 var pagerNav = document.querySelectorAll('.nav-links');
 
-pagerNav.forEach(function(el) {
-    el.classList.add('nav');
-});
+if (pagerNav.length) {
+
+    pagerNav.forEach(function (el) {
+        el.classList.add('nav');
+    });
+
+};
+
 
 var pagerNavLink = document.querySelectorAll('.nav-links .page-numbers');
 
-pagerNavLink.forEach(function(el) {
-    el.classList.add('nav-link');
-});
+if (pagerNavLink.length) {
+
+    pagerNavLink.forEach(function (el) {
+        el.classList.add('nav-link');
+    });
+
+};
 
 
 // --- Wordpress code ---
 
 var commentBox = document.querySelector('.comment-form-comment textarea');
 
-commentBox.removeAttribute('cols');
+if (commentBox) {
+
+    commentBox.removeAttribute('cols');
+
+};
