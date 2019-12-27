92eebea756613e7f730c72e327e90eba58e3de75
stefano-b <bartoletti.stefano@gmail.com>
2019-12-27 13:52:03 +0100

    customizer fields > echo only if not empty

--- templates/footer/footer-copyright.php
+++ templates/footer/footer-copyright.php
@@ -1,16 +1,20 @@
 <div id="footer-copyright" class="navbar navbar-expand navbar-dark container flex-column flex-md-row small">
                
     <span class="navbar-text">
-        <?php echo 
-        "&#169; ".date('o')." - ".
-        get_theme_mod('sb_company')." - ".
-        get_theme_mod('sb_address_1').", ".
-        get_theme_mod('sb_address_2')." - ".
-        "c.f. ".get_theme_mod('sb_cfisc')." - ".
-        "p.i. ".get_theme_mod('sb_piva')
+
+        <?php       
+        echo "&#169; ".date('o');
+        if( get_theme_mod('sb_company')) { echo ' | ', get_theme_mod('sb_company'); };
+        if( get_theme_mod('sb_address_1')) { echo ' | ', get_theme_mod('sb_address_1'); };
+        if( get_theme_mod('sb_address_2')) { echo ', ', get_theme_mod('sb_address_2'); };
+        if( get_theme_mod('sb_cfisc')) { echo ' | c.f. ', get_theme_mod('sb_cfisc'); };
+        if( get_theme_mod('sb_piva')) { echo ' | p.i. ', get_theme_mod('sb_piva'); };
         ?>
+        
     </span>
 
     <?php sb_signature('logo-small'); ?>
    
 </div> <!-- #footer-copyright --> 
+
+
