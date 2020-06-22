52608a1697f4ec7a33a2f3aa99550ff4f69f4e4d
stefano-b <bartoletti.stefano@gmail.com>
2020-06-22 15:51:03 +0200

    updated rollup

--- gulpfile.js/js.js
+++ gulpfile.js/js.js
@@ -5,10 +5,10 @@ const { src, dest } = require('gulp');
 
 // --- Plugins ---
 
-const rollup = require('gulp-better-rollup');
-const babel = require('rollup-plugin-babel');
+const rollup = require('gulp-rollup-each');
+const babel = require('@rollup/plugin-babel').babel;
 const commonjs = require('@rollup/plugin-commonjs');
-const resolve = require('@rollup/plugin-node-resolve');
+const resolve = require('@rollup/plugin-node-resolve').nodeResolve;
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
@@ -31,7 +31,7 @@ const project = require('../config/project');
 function js() {
     return src(config.js.src)
         .pipe(development(sourcemaps.init()))
-        .pipe(rollup({plugins: [babel(), commonjs(), resolve()]}, 'umd'))
+        .pipe(rollup({plugins: [babel({ babelHelpers: 'bundled' }), commonjs(), resolve()]},{ format: 'umd' }))
         .pipe(production(uglify()))
         .pipe(rename({
             suffix: '.min'
