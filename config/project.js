9193a2014e38a95afd6f09c6964576cc4bae90a2
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2020-10-01 12:45:12 +0200

    version

--- config/project.js
+++ config/project.js
@@ -3,7 +3,7 @@ module.exports = {
     // --- Project details ---
 
     name: 'Bricks',
-    version: '0.14.0',
+    version: '0.14.1',
     URL: '',
     desc: 'A modular starter theme built with Boostrap.',
     author: 'Stefano Bartoletti',
