ec164e779cef50da3d660b11c4d79ea3bede15bd
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2021-02-02 22:12:46 +0100

    bootstrap cdn

--- functions/cssjs.php
+++ functions/cssjs.php
@@ -19,7 +19,7 @@ if(! function_exists('brk_styles_scripts')) {
                
         wp_enqueue_script('fontawesome', get_template_directory_uri() .'/dist/js/fa5.min.js', false, null, true );
 
-        wp_enqueue_script('bootstrap-bundle', '//stackpath.bootstrapcdn.com/bootstrap/4.5.3/js/bootstrap.bundle.min.js', array('jquery'), null, true );
+        wp_enqueue_script('bootstrap-bundle', '//cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true );
 
         if ( is_singular() && comments_open() && get_option('thread_comments') ) { wp_enqueue_script('comment-reply'); }
 
