6e51151e136eb445517c7e2c88ff5d42ddc4f710
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2021-06-03 22:05:55 +0200

    update navbar container

--- templates/footer/footer-copyright.php
+++ templates/footer/footer-copyright.php
@@ -1,35 +1,39 @@
-<div id="footer-copyright" class="navbar navbar-expand navbar-dark container flex-column flex-md-row small">
+<div id="footer-copyright" class="navbar navbar-expand navbar-dark">
                
-    <span class="navbar-text">
+    <div class="container flex-column flex-md-row small">
+               
+        <span class="navbar-text">
 
-        <?php if ( class_exists('ACF') ) {
+            <?php if ( class_exists('ACF') ) {
 
-                $company = get_field('contacts_company', 'option');
-                $address1 = get_field('contacts_address_1', 'option');
-                $address2 = get_field('contacts_address_2', 'option');
-                $mapurl = get_field('contacts_map_url', 'option');
-                $phone = get_field('contacts_phone', 'option');
-                $email = get_field('contacts_email', 'option');
-                $idnumber = get_field('contacts_id_number', 'option');
-                $vatnumber = get_field('contacts_vat_number', 'option');  
+                    $company = get_field('contacts_company', 'option');
+                    $address1 = get_field('contacts_address_1', 'option');
+                    $address2 = get_field('contacts_address_2', 'option');
+                    $mapurl = get_field('contacts_map_url', 'option');
+                    $phone = get_field('contacts_phone', 'option');
+                    $email = get_field('contacts_email', 'option');
+                    $idnumber = get_field('contacts_id_number', 'option');
+                    $vatnumber = get_field('contacts_vat_number', 'option');  
 
-                echo "&#169; ".date('o'); 
-                echo $company ? ' | ' . $company : '';
-                echo $address1 ? ' | ' . $address1 : '';
-                echo $address2 ? ', ' . $address2 : '';
-                echo $idnumber ? ' | c.f. ' . $idnumber : '';
-                echo $vatnumber ? ' | p.i. ' . $vatnumber : '';
+                    echo "&#169; ".date('o'); 
+                    echo $company ? ' | ' . $company : '';
+                    echo $address1 ? ' | ' . $address1 : '';
+                    echo $address2 ? ', ' . $address2 : '';
+                    echo $idnumber ? ' | c.f. ' . $idnumber : '';
+                    echo $vatnumber ? ' | p.i. ' . $vatnumber : '';
+                
+                } ?>
             
-            } ?>
-        
-    </span>
+        </span>
 
-    <span id="brk-signature" class="navbar-text ms-md-auto">
-        <a class="text-white-50" href="https://github.com/stefanobartoletti/bricks" target="_blank">
-            Made with <?php echo file_get_contents(get_template_directory().'/dist/img/logo-small.svg') ?> Bricks
-        </a>
-    </span>
+        <span id="brk-signature" class="navbar-text ms-md-auto">
+            <a class="text-white-50" href="https://github.com/stefanobartoletti/bricks" target="_blank">
+                Made with <?php echo file_get_contents(get_template_directory().'/dist/img/logo-small.svg') ?> Bricks
+            </a>
+        </span>
    
+    </div>
+    
 </div> <!-- #footer-copyright --> 
 
 
--- templates/footer/footer-simple.php
+++ templates/footer/footer-simple.php
@@ -1,24 +1,28 @@
-<div id="footer-simple" class="navbar navbar-expand navbar-dark container flex-column flex-md-row my-3">
+<div id="footer-simple" class="navbar navbar-expand navbar-dark">
                
-    <?php 
-    
-    if ( has_custom_logo() ) { 
+    <div class="container flex-column flex-md-row my-3">
+               
+        <?php 
+        
+        if ( has_custom_logo() ) { 
 
-        the_custom_logo();
+            the_custom_logo();
 
-    }
+        }
 
-    wp_nav_menu(array(
-        'theme_location'    => 'footer',
-        'depth'             => 1,
-        'container'         => 'nav',
-        'container_class'   => 'mt-5 mt-md-0 ms-md-auto',
-        'container_id'      => '',
-        'menu_class'        => 'footer-menu navbar-nav',
-        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-        'walker'            => new WP_Bootstrap_Navwalker(),
-    ));
-    
-    ?>
+        wp_nav_menu(array(
+            'theme_location'    => 'footer',
+            'depth'             => 1,
+            'container'         => 'nav',
+            'container_class'   => 'mt-5 mt-md-0 ms-md-auto',
+            'container_id'      => '',
+            'menu_class'        => 'footer-menu navbar-nav',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker(),
+        ));
+        
+        ?>
+   
+    </div>
    
 </div> <!-- #footer-simple --> 
