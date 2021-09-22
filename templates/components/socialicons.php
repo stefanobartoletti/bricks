2fb675a513f8ab843a237f6fa646af9fad7a6885
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2021-09-22 18:50:27 +0200

    escape output

--- templates/components/socialicons.php
+++ templates/components/socialicons.php
@@ -9,8 +9,8 @@
 		if ( get_field( 'social_' . $key, 'option' ) ) { ?>
 
 			<li class="socialicon nav-item">
-				<a class="nav-link p-2" href="<?php echo get_field( 'social_' . $key, 'option' ); ?>" title="<?php echo $value['social-name']; ?>" target="_blank">
-					<i class="<?php echo $value['icon-style'] . ' ' . $value['icon-name']; ?>"></i>
+				<a class="nav-link p-2" href="<?php echo esc_attr( get_field( 'social_' . $key, 'option' ) ); ?>" title="<?php echo esc_attr( $value['social-name'] ); ?>" target="_blank">
+					<i class="<?php echo esc_attr( $value['icon-style'] ) . ' ' . esc_attr( $value['icon-name'] ); ?>"></i>
 				</a>
 			</li>
 	
