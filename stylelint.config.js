c3a91a67ed3c35e74eeda1544bae81bd16174b35
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2021-10-24 12:43:12 +0200

    fix config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
+    'stylelint-config-standard-scss',
     'stylelint-config-recess-order'
   ],
-  plugins: [
-    'stylelint-scss'
-  ],
+  plugins: [],
   rules: {
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
+    'scss/double-slash-comment-empty-line-before': null,
     'string-quotes': 'single'
   }
 };
