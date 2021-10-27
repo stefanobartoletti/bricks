c4e3fb08ce91e2dacf23b6f7081994a0f6c5e7d5
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2021-10-27 18:22:02 +0200

    add navbars

--- pattern-library.php
+++ pattern-library.php
@@ -15,6 +15,11 @@ $bs_colors = array(
 	'info',
 	'warning',
 	'danger',
+);
+
+$bs_schemes = array(
+	'light',
+	'dark',
 )
 
 ?>
@@ -97,7 +102,6 @@ $bs_colors = array(
 					<button type="button" class="btn btn-lg btn-outline-<?php echo $color; ?>">Button</button>
 				</div>
 
-
 				<div class="mb-4">
 					<button type="button" class="btn btn-sm btn-<?php echo $color; ?>">Button</button>
 					<button type="button" class="btn btn-sm btn-outline-<?php echo $color; ?>">Button</button>
@@ -108,6 +112,61 @@ $bs_colors = array(
 			<?php } ?>
 
 		</div> <!-- #buttons -->
+
+		<div id="navbars" class="row pt-5">
+
+			<h2 class="border-bottom mb-5">Navbars</h2>
+			
+			<div class="col-12 pb-5">
+
+			<?php foreach ( $bs_schemes as $scheme ) { ?>
+
+				<nav class="navbar navbar-expand-lg navbar-<?php echo $scheme; ?> bg-<?php echo $scheme; ?> mb-5">
+					<div class="container-fluid">
+						<a class="navbar-brand" href="#">Navbar</a>
+						<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+							data-bs-target="#navbarSupportedContent<?php echo '-' . $scheme; ?>" aria-controls="navbarSupportedContent<?php echo '-' . $scheme; ?>" aria-expanded="false"
+							aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarSupportedContent<?php echo '-' . $scheme; ?>">
+							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="#">Home</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">Link</a>
+								</li>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown<?php echo '-' . $scheme; ?>" role="button"
+										data-bs-toggle="dropdown" aria-expanded="false">
+										Dropdown
+									</a>
+									<ul class="dropdown-menu" aria-labelledby="navbarDropdown<?php echo '-' . $scheme; ?>">
+										<li><a class="dropdown-item" href="#">Action</a></li>
+										<li><a class="dropdown-item" href="#">Another action</a></li>
+										<li>
+											<hr class="dropdown-divider">
+										</li>
+										<li><a class="dropdown-item" href="#">Something else here</a></li>
+									</ul>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link disabled">Disabled</a>
+								</li>
+							</ul>
+							<form class="d-flex">
+								<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+								<button class="btn btn-outline-success" type="submit">Search</button>
+							</form>
+						</div>
+					</div>
+				</nav>
+
+
+			<?php } ?>
+
+		</div> <!-- #navbars -->
 
 	</div>
 
