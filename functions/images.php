c0bdcf1c9543ee4f92e13e36996d01dc5897d835
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-05-17 15:53:16 +0200

    add functions

--- functions/images.php
+++ functions/images.php
@@ -8,13 +8,26 @@
  * y_crop_position > top center bottom
  */
 
-add_image_size( 'brk_big', 1400, 800, true );
-add_image_size( 'brk_square', 400, 400, true );
-add_image_size( 'brk_single', 800, 500, true );
+if ( ! function_exists( 'brk_image_settings' ) ) {
+	function brk_image_settings() {
+		add_image_size( 'brk_big', 1400, 800, true );
+		add_image_size( 'brk_square', 400, 400, true );
+		add_image_size( 'brk_single', 800, 500, true );
+		// remove_image_size('large');
+		// remove_image_size('thumbnail');
+		// remove_image_size('medium');
+		// remove_image_size('medium_large');
+		remove_image_size( '1536x1536' );
+		remove_image_size( '2048x2048' );
+	}
+}
+add_action( 'after_setup_theme', 'brk_image_settings' );
 
-// remove_image_size('large');
-// remove_image_size('thumbnail');
-// remove_image_size('medium');
-// remove_image_size('medium_large');
-remove_image_size( '1536x1536' );
-remove_image_size( '2048x2048' );
+
+// --- Set image compression value ---
+// https://developer.wordpress.org/reference/hooks/jpeg_quality/
+
+// function brk_image_quality() {
+// 	return 80;
+// }
+// add_filter( 'jpeg_quality', 'brk_image_quality' );
