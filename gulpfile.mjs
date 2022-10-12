71f4ac6ad0b5d094c71e556bc01547d0e6c6c0c6
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-10-12 14:31:30 +0200

    add sass glob import

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -11,6 +11,7 @@ import config from './bricks.config.mjs';
 // CSS
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import sassGlob from '@artprog/gulp-sass-glob';
 import autoprefixer from 'gulp-autoprefixer';
 import purgecss from 'gulp-purgecss';
 import cleancss from 'gulp-clean-css';
@@ -59,6 +60,7 @@ const production = environments.production;
 function css () {
   return src(config.css.src)
     .pipe(development(sourcemaps.init()))
+    .pipe(sassGlob())
     .pipe(sass({
       quietDeps: true
     }).on('error', sass.logError))
